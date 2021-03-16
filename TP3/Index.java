b988878b9154ed4099b84b3b533347b305c68ba0
Aubertin Emmanuel <aubertin.emmanuel@gmail.com>
2021-03-16 10:33:41 +0100

    Before debug/test

--- TP3/Index.java
+++ TP3/Index.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * 
  */
@@ -14,18 +16,25 @@ public class Index {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
 
+	/**
+	 * @param maxSubscriber
+	 */
 	public Index(int maxSubscriber) {
 		this.NUMBER_OF_SUB = 0;
 		this.arraySubscriber = new Subscriber[maxSubscriber];
 	}
 	
+	/**
+	 * @param inputSub
+	 * @return
+	 */
 	public Boolean addSubscriber(Subscriber inputSub) {
-		if(inputSub != null && NUMBER_OF_SUB < this.arraySubscriber.length ) {
+		if(inputSub != null && NUMBER_OF_SUB < this.arraySubscriber.length && this.getNumber(inputSub.getName()) == -1 ) {
 			this.NUMBER_OF_SUB++;
 			this.arraySubscriber[NUMBER_OF_SUB] = inputSub;
 			return true;
@@ -34,6 +43,10 @@ public class Index {
 		}
 	}
 	
+	/**
+	 * @param inputName
+	 * @return
+	 */
 	public int getNumber(String inputName) {
 		for(int i = 0; i < this.NUMBER_OF_SUB; i++) {
 			if(this.arraySubscriber[i].getName() == inputName ) {
@@ -44,15 +57,37 @@ public class Index {
 	}
 
 
+	/**
+	 * @return
+	 */
 	public int getNbSubscribers() {
 		return NUMBER_OF_SUB;
 	}
 
 
+	/**
+	 * @param i
+	 * @return
+	 */
 	public Subscriber getSubscriber(int i) {
 		return this.arraySubscriber[i];
 	}
 
-
+	/**
+	 * @return
+	 */
+	public Subscriber[] getSortedSubscribers() {
+		Subscriber[] output = Arrays.copyOf(this.arraySubscriber, this.NUMBER_OF_SUB);
+		for (int i = 0; i < this.NUMBER_OF_SUB-1; i++) {
+            for (int j = 0; j < this.NUMBER_OF_SUB-i-1; j++) {
+            	if (output[j].getName().compareTo(output[j+1].getName()) >= 0){
+            		Subscriber tempSub = output[j];
+            		output[j] = output[j+1];
+            		output[j+1] = tempSub;
+            	}
+            }
+		}
+		return output;
+	}
 	
 }
